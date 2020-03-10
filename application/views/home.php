91992a252542bab7f0f9825793b3cf99251e5c7b
SureNamedWang <alphabetasatu@hotmail.com>
2020-03-11 06:35:07 +0700

    Add files via upload

---
+++ application/views/home.php
@@ -0,0 +1,12 @@
+<div class="main-panel">
+    <div class="content">
+        <div class="page-inner">
+            <div class="page-header">
+                <h4 class="page-title">Dashboard</h4>
+            </div>
+            <div class="row">
+                Halo Dunia apa kabarmu hari ini  
+            </div>
+        </div>
+    </div>
+</div>
